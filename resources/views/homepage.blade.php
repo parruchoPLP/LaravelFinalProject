9da9b1c65c182f8deb40769adb9724c00c517b50
arnauPLP <164654156+arnauPLP@users.noreply.github.com>
2024-05-31 01:06:38 +0800

    new file:   public/images/applelogo.png new file:   public/images/huaweilogo.jpg new file:   public/images/huaweiphones.png new file:   public/images/iphones.jpg new file:   public/images/realmelogo.png new file:   public/images/realmephones.jpg new file:   public/images/xiaomilogo.png new file:   public/images/xiaomiphones.jpg modified:   resources/views/homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,15 +7,15 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 overflow-x-hidden">
-    <nav class="bg-custom-darkBlue py-4">
+    <nav class="bg-custom-darkBlue py-4 fixed top-0 w-full z-50">
         <div class="w-full container mx-10 flex justify-between items-center">
             <ul class="flex space-x-6 text-white">
                 <li><a href="#" class="text-white text-2xl font-bold">logo</a></li>
                 <li><a href="#" class="text-white text-2xl font-bold">KeyPhone</a></li>
             </ul>
             <ul class="flex space-x-6 text-white w-16">
-                <li><a href="#" class="hover:border-b hover:border-custom-gold">Home</a></li>
-                <li><a href="#" class="hover:border-b hover:border-custom-gold">Store</a></li>
+                <li><a href="homepage" class="hover:border-b hover:border-custom-gold">Home</a></li>
+                <li><a href="storepage" class="hover:border-b hover:border-custom-gold">Store</a></li>
                 <li><a href="#" class="hover:border-b hover:border-custom-gold">Support</a></li>
                 <li><a href="#" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" class="ml-4">
@@ -47,7 +47,7 @@
     <section class="relative bg-cover bg-center" style="background-image: url('images/phones-bg.jpeg');">
         <div class="bg-custom-darkBlue bg-opacity-75 text-white py-52 flex flex-wrap items-center justify-center p-12 relative z-20">
             <div class="container mx-auto text-center md:w-1/2 lg:w-2/3">
-                <div class="text-center">
+                <div class="text-center m-8">
                     <h1 class="text-5xl font-bold mb-7">Welcome to KeyPhone</h1>
                     <p class="text-xl">Welcome to KeyPhone, your premier destination for the latest and greatest in mobile technology. Discover our extensive selection of cutting-edge smartphones, designed to keep you connected and ahead of the curve. At KeyPhone, we combine quality with affordability, ensuring you get the best value for your money. Join our community of tech enthusiasts today and experience the future of mobile communication. Shop now and unlock the power of innovation with KeyPhone!</p>
                     <a href="#" class="mt-6 inline-block bg-blue-700 hover:bg-custom-blueGray text-white font-bold py-3 px-8 rounded">Shop Now</a>
@@ -55,7 +55,7 @@
             </div>
         </div>
     </section>
-    <section class="bg-white py-3">
+    <section class="bg-white py-4">
         <div class="container mx-auto">
             <div class="flex justify-center items-center space-x-8">
                 <img src="{{ asset('images/apple.png') }}" alt="Apple" style="width: 180px; height: 150px;">
@@ -68,6 +68,35 @@
             </div>
         </div>
     </section>
+    <section class="relative">
+    <div class="bg-custom-darkBlue bg-opacity-95 text-white py-52 flex flex-wrap items-center justify-center p-12 relative z-20">
+        <div class="container mx-auto text-center md:w-1/2 lg:w-2/3">
+            <div class="text-center">
+                <h1 class="text-5xl font-bold mb-7">KeyPhone: Your Future in Your Pocket.</h1>
+                <div class="flex justify-center mb-10">
+                    <div class="w-1/4 lg:w-1/6 h-auto mx-4 rounded-lg overflow-hidden relative">
+                        <img src="{{ asset('images/iphones.jpg') }}" alt="Apple" class="h-full w-full object-cover">
+                        <img src="{{ asset('images/applelogo.png') }}" alt="Apple Hover" class="h-full w-full object-cover absolute inset-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
+                    </div>
+                    <div class="w-1/4 lg:w-1/6 h-auto mx-4 rounded-lg overflow-hidden relative">
+                        <img src="{{ asset('images/xiaomiphones.jpg') }}" alt="Xiaomi" class="h-full w-full object-cover">
+                        <img src="{{ asset('images/xiaomilogo.png') }}" alt="Xiaomi Hover" class="h-full w-full object-cover absolute inset-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
+                    </div>
+                    <div class="w-1/4 lg:w-1/6 h-auto mx-4 rounded-lg overflow-hidden relative">
+                        <img src="{{ asset('images/huaweiphones.png') }}" alt="Huawei" class="h-full w-full object-cover">
+                        <img src="{{ asset('images/huaweilogo.jpg') }}" alt="Huawei Hover" class="h-full w-full object-cover absolute inset-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
+                    </div>
+                    <div class="w-1/4 lg:w-1/6 h-auto mx-4 rounded-lg overflow-hidden relative">
+                        <img src="{{ asset('images/realmephones.jpg') }}" alt="Realme" class="h-full w-full object-cover">
+                        <img src="{{ asset('images/realmelogo.png') }}" alt="Realme Hover" class="h-full w-full object-cover absolute inset-0 opacity-0 hover:opacity-100 transition duration-300 ease-in-out">
+                    </div>
+                </div>
+                <p class="text-xl">Looking to elevate your mobile experience? Look no further! At KeyPhone, we've got what you're looking for. Dive into our range of sleek and stylish smartphones, each packed with features tailored to suit every lifestyle. Whether you crave seamless connectivity or crave stunning camera capabilities, KeyPhone delivers it all. Don't settle for less when you can have the best. Shop now and make every call, text, and selfie count with KeyPhone - Your Future in Your Pocket.</p>
+                <a href="#" class="mt-6 inline-block bg-blue-700 hover:bg-custom-blueGray text-white font-bold py-3 px-8 rounded">Shop Now</a>
+            </div>
+        </div>
+    </div>
+    </section>
     <section class="py-20">
         <div class="container mx-auto">
             <h2 class="text-3xl font-bold mb-8">Featured Phones</h2>
@@ -78,25 +107,25 @@
                         'name' => 'Product 1',
                         'price' => 'Php 999',
                         'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                        'image' => 'https://via.placeholder.com/500',
+                        'image' => asset('images/realmelogo.png'),
                     ],
                     [
                         'name' => 'Product 2',
                         'price' => 'Php 1299',
                         'description' => 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-                        'image' => 'https://via.placeholder.com/500',
+                        'image' => asset('images/realmelogo.png'),
                     ],
                     [
                         'name' => 'Product 3',
                         'price' => 'Php 1499',
                         'description' => 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-                        'image' => 'https://via.placeholder.com/500',
+                        'image' => asset('images/realmelogo.png'),
                     ],
                     [
                         'name' => 'Product 4',
                         'price' => 'Php 1799',
                         'description' => 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                        'image' => 'https://via.placeholder.com/500',
+                        'image' => asset('images/realmelogo.png'),
                     ],
                 ];
                 
@@ -128,14 +157,34 @@
             <div class="ml-auto -mr-10">
                 <h2 class="text-xl font-bold">Follow Us</h2>
                 <div class="flex mt-2">
-                    <a href="#" class="mr-4">Facebook</a>
-                    <a href="#" class="mr-4">Twitter</a>
-                    <a href="#">Instagram</a>
+                    <a href="http://facebook.com" class="mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="32" height="32" viewBox="0 0 32 32" version="1.1">
+                            <path d="M30.996 16.091c-0.001-8.281-6.714-14.994-14.996-14.994s-14.996 6.714-14.996 14.996c0 7.455 5.44 13.639 12.566 14.8l0.086 0.012v-10.478h-3.808v-4.336h3.808v-3.302c-0.019-0.167-0.029-0.361-0.029-0.557 0-2.923 2.37-5.293 5.293-5.293 0.141 0 0.281 0.006 0.42 0.016l-0.018-0.001c1.199 0.017 2.359 0.123 3.491 0.312l-0.134-0.019v3.69h-1.892c-0.086-0.012-0.185-0.019-0.285-0.019-1.197 0-2.168 0.97-2.168 2.168 0 0.068 0.003 0.135 0.009 0.202l-0.001-0.009v2.812h4.159l-0.665 4.336h-3.494v10.478c7.213-1.174 12.653-7.359 12.654-14.814v-0z"/>
+                        </svg>
+                    </a>
+                    <a href="http://twitter.com" class="mr-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32" height="32" viewBox="0 -2 20 20" version="1.1">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7521.000000)" fill="#ffffff">
+                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                            <path d="M10.29,7377 C17.837,7377 21.965,7370.84365 21.965,7365.50546 C21.965,7365.33021 21.965,7365.15595 21.953,7364.98267 C22.756,7364.41163 23.449,7363.70276 24,7362.8915 C23.252,7363.21837 22.457,7363.433 21.644,7363.52751 C22.5,7363.02244 23.141,7362.2289 23.448,7361.2926 C22.642,7361.76321 21.761,7362.095 20.842,7362.27321 C19.288,7360.64674 16.689,7360.56798 15.036,7362.09796 C13.971,7363.08447 13.518,7364.55538 13.849,7365.95835 C10.55,7365.79492 7.476,7364.261 5.392,7361.73762 C4.303,7363.58363 4.86,7365.94457 6.663,7367.12996 C6.01,7367.11125 5.371,7366.93797 4.8,7366.62489 L4.8,7366.67608 C4.801,7368.5989 6.178,7370.2549 8.092,7370.63591 C7.488,7370.79836 6.854,7370.82199 6.24,7370.70483 C6.777,7372.35099 8.318,7373.47829 10.073,7373.51078 C8.62,7374.63513 6.825,7375.24554 4.977,7375.24358 C4.651,7375.24259 4.325,7375.22388 4,7375.18549 C5.877,7376.37088 8.06,7377 10.29,7376.99705" id="twitter-[#154]"></path>
+                            </g>
+                            </g>
+                            </g>
+                    </svg>
+                    </a>
+                    <a href="http://instagram.com">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#ffffff"/>
+                            <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" fill="#ffffff"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" fill="#ffffff"/>
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="bg-gray-700 py-3 mt-4 mb-0">
+    <div class="bg-gray-700 py-3 mt-6 mb-0">
         <div class="container mx-auto text-center">
             <p>&copy; 2024 KeyPhone. All rights reserved.</p>
         </div>
